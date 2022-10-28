eb4172892813ac1d484ab46c4ba8c3dea15ff0ea
Your Name <7p5n52c@code.edu.az>
2022-10-28 10:04:48 +0400

    created script.js file

---
+++ script.js
@@ -0,0 +1,28 @@
+// let temp=0;
+// if (temp>15){
+//     console.log("Hava istidir");
+// }
+// else if (temp<15) 
+// {
+//     console.log("Hava soyuqdur");
+// }
+// let num=2;
+// if (num%2==0 ){
+//     console.log("Eded cutdur");
+// }
+// else if (num$%2==1)
+// {
+// console.log("Eded tekdir");
+let num1=2;
+let num2=3;
+let num3=4;
+if (num1>num3 && num2>num3){
+console.log("Num3 kicikdir");
+}
+else if (num1>num2 && num3>num2){
+    console.log("Num2 kicikdir");
+}
+else if (num3>num1 && num2>num1){
+    console.log("Num1 kicikdir");
+}
+
